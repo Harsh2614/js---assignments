6e6dc7d70593e92fdcc8d470c9738cecad5d17bb
Harsh Saxena <66464896+Harsh2614@users.noreply.github.com>
2020-07-16 14:22:17 +0530

    Create 3.loop.js

---
+++ Assignment - 3(Day 2)/scripts/3.loop.js
@@ -0,0 +1,10 @@
+var text;
+var userAge = prompt("What's Your Age - Less Than 21 Or More Than 21?");
+if(userAge>21)
+{
+    console.log("You Can Drink");
+}
+else
+{
+    console.log("You Cannot Drink");
+}
