d7729cef73baf52e5d1055711b514c56477024c6
Harsh Saxena <66464896+Harsh2614@users.noreply.github.com>
2020-07-17 11:53:26 +0530

    Create 2.os-namever.js

---
+++ Assignment - 4(Day 3)/scripts/2.os-namever.js
@@ -0,0 +1,4 @@
+var os = prompt("What Is The Name & Current Version Of Your Operating System?");
+var res = os.split(" ");
+console.log("The OS Name Is " + res[0] + " And The OS Version Is " + res[1]);
+//console.log("The OS Version Is " + res[1]);
