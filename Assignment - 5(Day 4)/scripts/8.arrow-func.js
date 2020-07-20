8c8e70432cf6ce15074c751b68c60f4b40b45b6d
Harsh Saxena <66464896+Harsh2614@users.noreply.github.com>
2020-07-20 09:37:28 +0530

    Create 8.arrow-func.js

---
+++ Assignment - 5(Day 4)/scripts/8.arrow-func.js
@@ -0,0 +1,23 @@
+//Original Function Expressions:-
+
+/*function ask(question, yes, no) {
+    if(confirm(question)) yes()
+    else no();
+}
+
+ask(
+    "Do you agree?",
+    function() { alert("You agreed."); },
+    function() { alert("You canceled the execution."); }
+);*/
+
+
+//Replacing Them With Arrow Functions:-
+
+let ask = (question, yes, no) => confirm(question) ? yes() : no();
+
+ask(
+    "Do you agree?",
+    () => alert("You agreed"),
+    () => alert("You  canceled the execution.")
+);
